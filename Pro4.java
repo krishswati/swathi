48ca40c8e5a8f8236052a23ba6929b858f1a293c
krishswati <40380588+krishswati@users.noreply.github.com>
2018-06-19 13:25:35 +0000

    Create Pro4.java

---
+++ Pro4.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Pro4
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a/4==0)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
