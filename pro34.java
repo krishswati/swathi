a9de134d163f9706d8a6c5c31310a48160cd3e9f
krishswati <40380588+krishswati@users.noreply.github.com>
2018-06-21 03:46:35 +0000

    Create pro34.java

---
+++ pro34.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class pro34
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            System.out.println("Hello");
+        }
+        
+    }
+}
